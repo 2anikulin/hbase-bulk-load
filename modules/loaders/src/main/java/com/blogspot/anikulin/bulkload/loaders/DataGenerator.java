4fb1179689bb8d42cec8ee5c20f713f9e21c10b7
anikulin <anikulin@thumbtack.net>
2013-09-09 13:41:40 +0700

    generator changed

--- modules/loaders/src/main/java/com/blogspot/anikulin/bulkload/loaders/DataGenerator.java
+++ modules/loaders/src/main/java/com/blogspot/anikulin/bulkload/loaders/DataGenerator.java
@@ -1,12 +1,9 @@
 package com.blogspot.anikulin.bulkload.loaders;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 
-import java.io.File;
-import java.io.InputStream;
-import java.io.StringWriter;
+import java.io.*;
 
 /**
  * @author Anatoliy Nikulin
@@ -26,53 +23,54 @@ public class DataGenerator {
         }
 
         String outputFile = args[0];
-        long  rowsCount = Long.parseLong(args[1]);
-        long splitCount = args.length == 3 ? Long.parseLong(args[2]) : rowsCount;
+        long rowsCount = Long.parseLong(args[1]);
+        long splitRowsCount = args.length == 3 ? Long.parseLong(args[2]) : rowsCount;
 
         InputStream inputStream = null;
-        StringWriter writer = null;
+        StringWriter resourceWriter = null;
+        BufferedWriter fileWriter = null;
 
         try {
             inputStream = DataGenerator.class.getClassLoader().getResourceAsStream(ROW_DATA_FILE);
 
-            writer = new StringWriter();
-            IOUtils.copy(inputStream, writer);
-            String rowString = writer.toString();
+            resourceWriter = new StringWriter();
+            IOUtils.copy(inputStream, resourceWriter);
+            String rowString = resourceWriter.toString();
 
             System.out.println("Start");
 
-            StringBuilder builder = new StringBuilder();
-
             int counter = 0;
             for (long i = 0; i < rowsCount; i++) {
-                builder.append(
+
+                if (i % splitRowsCount == 0) {
+                    IOUtils.closeQuietly(fileWriter);
+                    fileWriter = createFile(outputFile, counter++);
+                }
+
+                fileWriter.write(
                         String.format("%d\t%s\n", i, rowString)
                 );
-
-                if (((i+1) % splitCount == 0) || ((i+1) == rowsCount)) {
-                    FileUtils.writeStringToFile(
-                           new File(getFileName(outputFile, counter++)),
-                           builder.toString()
-                    );
-                    builder = new StringBuilder();
-                }
             }
-
             System.out.println("Successfully!");
 
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
+            IOUtils.closeQuietly(fileWriter);
             IOUtils.closeQuietly(inputStream);
-            IOUtils.closeQuietly(writer);
+            IOUtils.closeQuietly(resourceWriter);
         }
     }
 
-    private static String getFileName(String file, int counter) {
-        return String.format("%s%d.%s",
-                FilenameUtils.removeExtension(file),
+
+    private static BufferedWriter createFile(String filePath, int counter) throws IOException {
+        String newFileName = String.format(
+                "%s%d.%s",
+                FilenameUtils.removeExtension(filePath),
                 counter,
-                FilenameUtils.getExtension(file)
+                FilenameUtils.getExtension(filePath)
         );
+
+        return new BufferedWriter(new FileWriter(newFileName));
     }
 }
